e892a3005021446271eb69e506f1acc9e388fc49
Knux <knuxfan@live.co.uk>
2026-06-21 13:57:04 +0100

    Event Animation Tweaks
    Turns out some events that Lilac uses just don't have her bool set to true????

--- Patchers/FPEventSequencePatcher.cs
+++ Patchers/FPEventSequencePatcher.cs
@@ -6,14 +6,14 @@
         [HarmonyPatch(typeof(FPEventSequence), "Action_StartEvent")]
         static void StupidAssEventAnimatorReplacement(FPEventSequence __instance)
         {
-            // If we're Sonic and this animator is for Lilac, then dig through the activate and deactivate arrays to find and swap the animators.
-            if (FPSaveManager.character == Plugin.sonicCharacterID && __instance.lilac)
+            // If we're Sonic dig through the activate and deactivate arrays to find and swap the animators.
+            if (FPSaveManager.character == Plugin.sonicCharacterID)
             {
                 foreach (FPBaseObject? obj in __instance.activateOnStart) HandleSwap(obj);
                 foreach (FPBaseObject? obj in __instance.deactivateOnEnd) HandleSwap(obj);
             }
 
-            void HandleSwap(FPBaseObject? obj)
+            static void HandleSwap(FPBaseObject? obj)
             {
                 // Don't bother doing anything if we've somehow ended up with a null object.
                 if (obj == null) return;
@@ -32,9 +32,9 @@
                             obj.transform.GetChild(0).gameObject.SetActive(false);
 
                         // Swap to Sonic's event animator.
-                        // TODO: Actually make the Super one.
+                        // TODO: Super Sonic is missing some poses so he reverts to normal in that case.
                         if (!FPPlayerPatcher.isSuper) animator.runtimeAnimatorController = Plugin.sonicAssetBundle.LoadAsset<RuntimeAnimatorController>("Event Sonic Animator");
-                        else animator.runtimeAnimatorController = Plugin.sonicAssetBundle.LoadAsset<RuntimeAnimatorController>("Event Sonic Animator");
+                        else animator.runtimeAnimatorController = Plugin.sonicAssetBundle.LoadAsset<RuntimeAnimatorController>("Event Super Sonic Animator");
                     }
                 }
             }
