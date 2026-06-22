50b1db6f2a5e14e7fea1200da216f297048a102f
Knux <knuxfan@live.co.uk>
2026-06-22 06:48:22 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ namespace FP2_Sonic_Mod
     TODO: Try and make the Homing Attack less likely to send Sonic through solid matter.
     TODO: Maybe try and rewrite the Homing Attack in such a way that it can lock on to item boxes as well.
     TODO: Update the tutorial to reflect tweaks to Sonic's playstyle.
-    TODO: The Shadow Guard is broken. Why?
+    TODO: The Shadow Guard is broken as it uses the animation names rather than the state names, how do we fix this cleanly? Hitbox is also borked.
     */
     [BepInPlugin("K24_FP2_Sonic", "Sonic The Hedgehog", "1.1.0")]
     [BepInDependency("000.kuborro.libraries.fp2.fp2lib")]
